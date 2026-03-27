832698eb18f09ef290468b46b511867bf38cd1b5
kim taeyun <ktywantrich@gmail.com>
2026-03-27 22:37:01 +0900

    Revert "fix: print warnings to stderr so they show without -v flag"
    This reverts commit 93ffe7fba9e021943b088a94531ccc43d75a1175.

--- report/report.go
+++ report/report.go
@@ -1,8 +1,6 @@
 package report
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/NamhaeSusan/go-arch-guard/rules"
@@ -10,15 +8,12 @@ import (
 
 // AssertNoViolations prints all violations.
 // Fails the test only if any ERROR-level violations exist.
-// Warnings are printed to stderr so they are always visible without -v.
 func AssertNoViolations(t testing.TB, violations []rules.Violation) {
 	t.Helper()
 	hasErrors := false
 	for _, v := range violations {
-		if v.Severity == rules.Warning {
-			fmt.Fprintln(os.Stderr, v.String())
-		} else {
-			t.Log(v.String())
+		t.Log(v.String())
+		if v.Severity == rules.Error {
 			hasErrors = true
 		}
 	}
